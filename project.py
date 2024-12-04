090e2bb1c6361aeb03496b3b988f95d50ce92ce9
Edison Kardirsson <ed.kardirsson@hotmail.com>
2024-12-04 15:28:28 +0100

    Get table data

--- project.py
+++ project.py
@@ -19,4 +19,23 @@ if not os.path.exists(path):
 with open(path, 'r', encoding='utf8') as f:
     html = f.read()
 
-print(html)
+soup = BeautifulSoup(html, 'html.parser')
+
+tables = soup.find_all('table', attrs={'class': 'wikitable sortable'})
+
+data = {}
+# for t in tables:
+#     heading_div = t.find_previous_sibling(['div', {'class': 'mw-heading'}])
+#     heading = heading_div.find(['h2', 'h3'])
+#     print(heading)
+
+for t in tables:
+    heading = None
+    for elem in t.find_all_previous(['h2', 'h3']):
+        if elem.parent.get('class')!= ['mw-parser-output']:
+            heading = elem.text
+            break
+    print(heading)
+    data[heading] = t
+
+print('\n', data['Australia'])
